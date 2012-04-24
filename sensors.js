0afb07c5755c5655f7bb8f1c3d0084ecef034a74
Luke Steensen <luke.steensen@gmail.com>
2012-04-24 13:23:56 -0500

    move sensor data to separate file

---
+++ sensors.js
@@ -0,0 +1,37 @@
+// parsers
+var parseTemp = function(str) {
+    return (((parseInt(str, 16) / 50 - 273.15) * 1.8 + 32).toFixed(2));
+};
+
+// sensors
+this.sensors = [
+    {
+        name: 'front-driver-tire-temp',
+        // cmd:    "i2cget -y 3 0x5a 0x07 w",
+        cmd:    'echo $RANDOM',
+        parser: parseTemp,
+        units:  " degrees F"
+    },
+    {
+        name: 'front-driver-ambient-temp',
+        // cmd:    "i2cget -y 3 0x5a 0x06 w",
+        cmd:    'echo $RANDOM',
+        parser: parseTemp,
+        units:  " degrees F"
+    },
+    {
+        name: 'front-passenger-tire-temp',
+        // cmd:    "i2cget -y 3 0x5b 0x07 w",
+        cmd:    'echo $RANDOM',
+        parser: parseTemp,
+        units:  " degrees F"
+    },
+    {
+        name: 'front-passenger-ambient-temp',
+        // cmd:    "i2cget -y 3 0x5b 0x06 w",
+        cmd:    'echo $RANDOM',
+        parser: parseTemp,
+        units:  " degrees F"
+    }
+];
+
--- server.js
+++ server.js
@@ -2,44 +2,8 @@
 var http = require('http'),
     exec = require('child_process').exec,
     stat = require('node-static'),
+    sensors = require('./sensors').sensors,
     faye = require('faye');
-
-// parsers
-var parseTemp = function(str) {
-    return (((parseInt(str, 16) / 50 - 273.15) * 1.8 + 32).toFixed(2));
-};
-
-// sensors
-var sensors = [
-    {
-        name: 'front-driver-tire-temp',
-        // cmd:    "i2cget -y 3 0x5a 0x07 w",
-        cmd:    'echo $RANDOM',
-        parser: parseTemp,
-        units:  " degrees F"
-    },
-    {
-        name: 'front-driver-ambient-temp',
-        // cmd:    "i2cget -y 3 0x5a 0x06 w",
-        cmd:    'echo $RANDOM',
-        parser: parseTemp,
-        units:  " degrees F"
-    },
-    {
-        name: 'front-passenger-tire-temp',
-        // cmd:    "i2cget -y 3 0x5b 0x07 w",
-        cmd:    'echo $RANDOM',
-        parser: parseTemp,
-        units:  " degrees F"
-    },
-    {
-        name: 'front-passenger-ambient-temp',
-        // cmd:    "i2cget -y 3 0x5b 0x06 w",
-        cmd:    'echo $RANDOM',
-        parser: parseTemp,
-        units:  " degrees F"
-    }
-];
 
 
 // static file server to serve the /public folder
