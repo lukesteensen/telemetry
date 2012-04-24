8d1fa19adb4d09ad6e8e1a27c2e16df754309976
Luke Steensen <luke.steensen@gmail.com>
2012-04-23 23:41:17 -0500

    add static server and move static assets

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // requires
-// require.paths.unshift('./node_modules');
 var http = require('http'),
     exec = require('child_process').exec,
+    stat = require('node-static'),
     faye = require('faye');
 
 // parsers
@@ -13,45 +13,59 @@ var parseTemp = function(str) {
 var sensors = [
     {
         label:  "ObjectA temp:  ",
-        cmd:    "i2cget -y 3 0x5a 0x07 w",
+        // cmd:    "i2cget -y 3 0x5a 0x07 w",
+        cmd:    'echo $RANDOM',
         parser: parseTemp,
         units:  " degrees F"
     },
     {
         label:  "AmbientA temp: ",
-        cmd:    "i2cget -y 3 0x5a 0x06 w",
+        // cmd:    "i2cget -y 3 0x5a 0x06 w",
+        cmd:    'echo $RANDOM',
         parser: parseTemp,
         units:  " degrees F"
     },
     {
         label:  "ObjectB temp:  ",
-        cmd:    "i2cget -y 3 0x5b 0x07 w",
+        // cmd:    "i2cget -y 3 0x5b 0x07 w",
+        cmd:    'echo $RANDOM',
         parser: parseTemp,
         units:  " degrees F"
     },
     {
         label:  "AmbientB temp: ",
-        cmd:    "i2cget -y 3 0x5b 0x06 w",
+        // cmd:    "i2cget -y 3 0x5b 0x06 w",
+        cmd:    'echo $RANDOM',
         parser: parseTemp,
         units:  " degrees F"
     }
 ];
 
 
-// the main server
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var callbacks = 0;
-    sensors.forEach(function(s) {
-        callbacks += 1;
-        exec(s.cmd, function (error, stdout, stderr) {
-            res.write( s.label + s.parser(stdout) + s.units + '\n' );
-            callbacks -= 1;
-            if (callbacks === 0) {
-                res.end();
-            }
-        });
-    });
-}).listen(1337, '');
+// static file server to serve the /public folder
+var fileServer = new stat.Server('./public');
 
-console.log('Server running on port 1337');
+// the main server
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  });
+}).listen(8080);
+
+// old server code
+// http.createServer(function (req, res) {
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // var callbacks = 0;
+    // sensors.forEach(function(s) {
+        // callbacks += 1;
+        // exec(s.cmd, function (error, stdout, stderr) {
+            // res.write( s.label + s.parser(stdout) + s.units + '\n' );
+            // callbacks -= 1;
+            // if (callbacks === 0) {
+                // res.end();
+            // }
+        // });
+    // });
+// }).listen(1337, '');
+
+console.log('Server running on port 8080');
